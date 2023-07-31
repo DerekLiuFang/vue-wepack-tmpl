b608a9d890458a08550ba959ed2c3941a861b237
DerekLiuFang <DerekLiuFang@outlook.com>
2023-07-31 12:59:16 +0800

    feat: 添加对图片&字体&媒体等静态资源处理

--- build/webpack.base.js
+++ build/webpack.base.js
@@ -1,6 +1,7 @@
 const path = require("node:path");
 const { VueLoaderPlugin } = require("vue-loader");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const Webpack = require("webpack");
 
 /**
  * @type {import('webpack').Configuration}
@@ -26,6 +27,47 @@ module.exports = {
         use: ["babel-loader"],
         exclude: /node_modules/,
       },
+      {
+        test: /\.(css|less)$/,
+        use: ["style-loader", "css-loader", "postcss-loader", "less-loader"],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/, // 匹配图片文件
+        type: "asset", // type选择asset, 在导出一个 data URI 和发送一个单独的文件之间自动选择
+        parser: {
+          dataUrlCondition: {
+            maxSize: 10 * 1024, // 小于10kb转base64位
+          },
+        },
+        generator: {
+          filename: "static/images/[name][ext]", // 文件输出目录和命名
+        },
+      },
+      {
+        test: /.(woff2?|eot|ttf|otf)$/, // 匹配字体图标文件
+        type: "asset", // type选择asset, 在导出一个 data URI 和发送一个单独的文件之间自动选择
+        parser: {
+          dataUrlCondition: {
+            maxSize: 10 * 1024, // 小于10kb转base64位
+          },
+        },
+        generator: {
+          filename: "static/fonts/[name][ext]", // 文件输出目录和命名
+        },
+      },
+
+      {
+        test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)$/, // 匹配媒体文件
+        type: "asset", // type选择asset, 在导出一个 data URI 和发送一个单独的文件之间自动选择
+        parser: {
+          dataUrlCondition: {
+            maxSize: 10 * 1024, // 小于10kb转base64位
+          },
+        },
+        generator: {
+          filename: "static/media/[name][ext]", // 文件输出目录和命名
+        },
+      },
     ],
   },
   plugins: [
@@ -33,6 +75,10 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "../public/index.html"),
       inject: true,
+    }),
+    new Webpack.DefinePlugin({
+      "process.env.BASE_ENV": JSON.stringify(process.env.BASE_ENV),
+      "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV),
     }),
   ],
   // 解析配置
--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,16 @@
 <template>
   <h2>vue-webpack-tmpl</h2>
+  <img :src="smallImg" alt="小于10kb的图片" />
+  <img :src="bigImg" alt="大于于10kb的图片" />
+  <!-- 小图片背景容器 -->
+  <div className="smallImg"></div>
+  <!-- 大图片背景容器 -->
+  <div className="bigImg"></div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import "./App.less";
+import bigImg from "./assets/imgs/22kb.png";
+import smallImg from "./assets/imgs/5kb.png";
+</script>
 <style scoped></style>
